04b62296b364b7b7ac721f55f8bd026966216e3e
Nguyễn Phúc Vinh <git@nguyenphucvinh.io.vn>
2026-08-14 16:01:19 +0700

    hello_cargo

---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/vinh/workspace/learn-rust/hello_cargo/target/debug/hello_cargo: /home/vinh/workspace/learn-rust/hello_cargo/src/main.rs
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/vinh/workspace/learn-rust/hello_cargo/target/release/hello_cargo: /home/vinh/workspace/learn-rust/hello_cargo/src/main.rs
