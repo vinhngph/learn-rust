b251acd042ab227a1747f1dd1daf13c3d3246c7a
Nguyễn Phúc Vinh <git@nguyenphucvinh.io.vn>
2026-09-07 12:06:13 +0700

    chapter 6.1

--- chapter_6_1/src/main.rs
+++ chapter_6_1/src/main.rs
@@ -16,6 +16,20 @@ enum IpAddress {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+// enum can have method
+impl Message {
+    fn call(&self) {
+        //
+    }
+}
+
 fn main() {
     let home = IpAddr {
         kind: IpAddrKind::V4,
@@ -25,4 +39,17 @@ fn main() {
 
     let loopback = IpAddress::V6(String::from("::1"));
     println!("{loopback:#?}");
+
+    let m = Message::Write(String::from("hello"));
+    m.call();
+
+    let some_number = Some(5);
+    let some_char = Some('a');
+    let absent_number: Option<i32> = None;
+
+    println!("some_number={some_number:?} some_char={some_char:?} absent_number={absent_number:?}");
+
+    let success: Result<i32, String> = Ok(42);
+    let error: Result<i32, String> = Err("something went wrong.".to_string());
+    println!("success={success:?} error={error:?}");
 }
