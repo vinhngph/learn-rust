12b8ffeafd870ebd21bec675a2546f37a3c75cb3
Nguyễn Phúc Vinh <git@nguyenphucvinh.io.vn>
2026-09-15 15:08:25 +0700

    Make hosting and serving modules public
    - Add `pub` to `hosting` and `serving` modules in `front_of_house`
    - Make `add_to_waitlist` function public within `hosting`
    - Add `deliver_order` function and `back_of_house` module
    - Add `eat_at_restaurant` function demonstrating absolute and relative
      path usage

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,15 +1,34 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
 
-    mod serving {
+    pub mod serving {
         fn take_order() {}
 
         fn serve_order() {}
 
         fn take_payment() {}
     }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path
+    front_of_house::hosting::add_to_waitlist();
 }
